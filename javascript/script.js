01d952d88d4d700141c9c7874caf06d53b34acbb
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-04-03 12:05:40 +0200

    start

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+import {contacts} from './data.js';
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            contacts: contacts,
+            activeContactId: 1
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    mounted(){
+        console.log(this.contacts);
+    }
+}).mount('#app');
